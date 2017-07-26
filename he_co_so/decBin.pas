b1a2773ab2d9c4221bedae307b8676e1abd05d5d
Thanh Kiệt <thanhkieta@gmail.com>
2017-07-26 21:11:09 +0700

    Create decBin.pas

---
+++ he_co_so/decBin.pas
@@ -0,0 +1,25 @@
+(*chuyển số ở hệ thập phân sang hệ nhị phân*)
+
+program gin;
+uses crt;
+var
+	dec : Integer;
+function decBin(d : Integer) : string;
+var 
+	chuoi : string;
+	a : string;
+begin 
+	chuoi := '';
+	repeat 
+		str(d mod 2,a);
+		insert(a, chuoi, 1);
+		d := d div 2;
+	until d = 0;
+	exit (chuoi);
+end;
+begin 
+	write('Nhap he tap phan : ');
+	readln(dec);
+	writeln(decBin(dec));
+	readln
+end.
