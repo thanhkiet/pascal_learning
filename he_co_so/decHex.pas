7cc0644c98bb75a97f75ac2223ecdf4bedfb7170
Thanh Kiệt <thanhkieta@gmail.com>
2017-07-26 21:13:33 +0700

    Create decHex.pas

---
+++ he_co_so/decHex.pas
@@ -0,0 +1,26 @@
+(*chuyển số ở hệ thập phân asg nhệ thập lục phân*)
+
+program gin;
+uses crt;
+const 
+	convern : array[0..15] of String = 
+	('0','1','2','3','4','5','6','7','8','9',
+		'A','B','C','D','E','F');
+var 
+	dec : Longint;
+function decHex(so : Longint) : string;
+var 
+	result : string = '';
+begin
+	repeat 
+		insert(convern[so mod 16], result,1);
+		so := so div 16;
+	until so = 0;
+	exit(result);
+end;
+begin 
+	write('Nhap so o he thap phan : ')
+	readln(dec);
+	writeln(decHex(dec));
+	readln
+end.
